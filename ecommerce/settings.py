c70a17165217592f1be9b92daf5f2b95affaccae
cxrtut <160480710+cxrtut@users.noreply.github.com>
2024-07-16 23:43:24 +0200

    allow host changeed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+
 
 
 # Application definition
